cefd8fd6e6c662647418c2f4a342486eeb89f8e9
Arusa Hussain <hussainarusa18@gmail.com>
2023-03-20 21:36:37 -0500

    "committing Konami code"

---
+++ js/konami.js
@@ -0,0 +1,14 @@
+
+    $(document).keypress({
+
+            keyCode: [38,38,40,40,37,39,39,37,66,65,13],
+        },
+    function() {
+        alert("You have added 30 lives!")
+    });
+
+
+
+
+
+
