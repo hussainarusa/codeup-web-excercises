f46ff4ac215ec32d9fdcf057863fb7b9d0011306
Arusa Hussain <hussainarusa18@gmail.com>
2023-06-05 19:14:02 -0500

    commiting the for loops js exercise

---
+++ js/for_loops.js
@@ -0,0 +1,52 @@
+
+    //2. Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication
+    // table for that number (just multiply by the numbers 1 through 10)
+   //
+   //  function showMultiplicationTable (number){
+   //  for(let i = 1; i <= 10; i++) {
+   //      let result = i * number;
+   //      console.log(`${number} * ${i} = ${result}`);
+   //  }}
+   //
+   // showMultiplicationTable(3)
+
+
+   //3. Use a for loop and the code from the previous lessons to generate 10 random
+   // numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+
+    // let randomNum;
+    //     for (let i = 1; i <= 10; i++){
+    //    randomNum = Math.floor((Math.random() * 20) + 200);
+    //     console.log(randomNum);
+    //    if (randomNum % 2 === 0 ){
+    //      console.log("the number is even")
+    //     }else{
+    //        console.log("the number is odd")
+    //     }}
+
+
+    // //4.Create a for loop that uses console.log to create the output shown below
+    // 1
+    // 22
+    // 333
+    // 4444
+    // 55555
+    // 666666
+    // 7777777
+    // 88888888
+    // 999999999
+
+    // for (let i = 1; i <= 9; i++) {
+    //     let row = `${i}`;
+    //     for(let j = i; j > 0; j--) {
+    //         let moreNumbers = `${i}`;
+    //         row += moreNumbers;
+    //     }
+    //          console.log(row);
+    //         }
+
+
+// 5.
+//     for (let i= 100; i>=5; --i){
+//         console.log(i)
+//     }
