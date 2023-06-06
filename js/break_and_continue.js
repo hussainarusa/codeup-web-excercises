48925a70eb86bb1a4c9bc3f34262e7f18f6e0d63
Arusa Hussain <hussainarusa18@gmail.com>
2023-06-06 11:00:52 -0500

    commiting break and continue loops exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,34 @@
+//1 Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+
+ // my attempt with a do while
+ //    let oddNumber;
+ //    do {
+ //        oddNumber = prompt("Please enter an odd number between 1 and 50")
+ //        if (oddNumber % 2 !== 0) {
+ //            alert(`You entered the odd number: ${oddNumber}`)
+ //            break
+ //        }   prompt ("Please enter an odd number between 1 and 50")
+ //    } while (oddNumber % 2 !== 0) ;
+
+
+//     let oddNumber;
+//     while (true){
+//          oddNumber = prompt("Please enter an odd number between 1 and 50")
+//             oddNumber = parseInt(oddNumber)
+//             if (oddNumber %2 !== 0 && oddNumber <= 50 && oddNumber >= 1 ){
+//                 alert(`You entered the odd number : ${oddNumber}`)
+//                 break;
+//             } else {
+//
+//         }
+//     }
+// //2
+//         for ( let i =1 ; i <= 50; i += 2){
+//             if( i === oddNumber){
+//                 console.log(`Yikes! Skipping number: ${oddNumber}`)
+//                     continue;
+//             }
+//              console.log(`Here is an odd number: ${i}`)
+//         }
+
+
