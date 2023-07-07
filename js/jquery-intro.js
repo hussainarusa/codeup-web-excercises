ad94151f3ae16d268bae748de0fd88b693e07475
Arusa Hussain <hussainarusa18@gmail.com>
2023-07-07 14:07:31 -0500

    chnaged the file name

---
+++ js/jquery-intro.js
@@ -0,0 +1,16 @@
+// window.onload=function(){
+//     alert("window has loaded")
+// } /// vanilla javascript
+
+
+// now with jquery
+// use jquery object
+// jQuery(function (){
+//     alert("oogie-boogie")
+// })
+//
+// slight difference is jQuery pops up before images load vjs will
+
+jQuery(function (){
+    alert("the DOM has loaded")
+})
