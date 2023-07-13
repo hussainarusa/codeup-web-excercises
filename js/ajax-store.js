7ee5383d9a22a091b3bfb97351737071f0e6699e
Arusa Hussain <hussainarusa18@gmail.com>
2023-07-13 11:57:05 -0500

    Updated ajax store Exercise with new code and styling

--- js/ajax-store.js
+++ js/ajax-store.js
@@ -1,25 +1,36 @@
-(function () {
+(async function() {
     "use strict";
-    // TODO: Create an AJAX GET request for the file under data/inventory.json
-
+    // TODO: Create a fetch request for the file under data/inventory.json
+    const getInventory = async () => {
+        let response = await fetch('/data/inventory.json');
+        let data = await response.json();
+        return data;
+    }
+    let inventory = await getInventory();
+    console.log(inventory);
 
     // TODO: Take the data from inventory.json and append it to the products table
     //       HINT: Your data should come back as a JSON object; use console.log() to inspect
     //             its contents and fields
     //       HINT: You will want to target #insertProducts for your new HTML elements
-
-    $.ajax("/data/inventory.json").done(function (data) {
-        console.log(data);
-        let html = " ";
-        data.forEach(function (product) {
-            html += "<tr>";
-            html += "<td>" + product.title + "</td>";
-            html += "<td>" + product.quantity + "</td>";
-            html += "<td>" + product.price + "</td>";
-            html += "<td>" + product.categories + "</td>";
-            html += "</tr>";
+    const buildTable = (inventory) => {
+        inventory.forEach(function(tool){
+            let row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${tool.title}</td>
+                <td>${tool.quantity}</td>
+                <td>${(tool.price)}</td>
+                <td>${(tool.categories)}</td>
+            `;
+            document.querySelector('#insertProducts').appendChild(row);
         });
-        $("#insertProducts").html(html);
+    }
+    buildTable(inventory);
+
+    document.querySelector('#refreshTable').addEventListener('click', async function(){
+        document.querySelector('#insertProducts').innerHTML = '';
+        let inventory = await getInventory();
+        buildTable(inventory);
     });
 
 
